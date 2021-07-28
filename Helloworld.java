e09889df0fb10147dbc6086aec2b56ca021cef61
coder-li <lizepengmail@163.com>
2021-07-28 15:33:53 +0800

    i commit hw

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("你错了");
+		System.out.println("不，我没有");
+		System.out.println("你错了");
+		System.out.println("是你的错，不是我");
+	}
+}
